20673d3c52c53f2a3f1530d512fd6f07d95d8fce
OakarminIron <oakarminiron@gmail.com>
2021-01-03 21:36:08 +0630

    Upgrade_packages

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -141,6 +141,7 @@ class _AuthCardState extends State<AuthCard> {
           _authData['password'],
         );
       }
+     // Navigator.of(context).pushReplacementNamed('/overview');
     } on HttpException catch (error) {
       var errorMessage = 'Authentication failed';
       if (error.toString().contains('EMAIL_EXISTS')) {
